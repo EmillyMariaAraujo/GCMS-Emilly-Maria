66e6f52442a89d9741672b74042b2ed1179bf6c1
Emilly Maria Araujo da Silva <emillymaria93@gmail.com>
2025-07-31 00:09:00 -0300

    Criação endpoint Hotfix

--- src/main/java/com/example/WebApp/HomeController.java
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -1,33 +1,41 @@
 package com.example.WebApp;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
-@RequestMapping("/")
-public @ResponseBody String greeting() {
-return "Hello, World!";
-}
-//testando 
-@RequestMapping("/Emilly")
-public @ResponseBody String greetingEmilly() {
-return "Hello, Emilly!";
-}
 
-@RequestMapping("/Ramide")
-public @ResponseBody String greetingRamide() {
-return "Hello, Ramide!";
-}
-//testando 
+    @RequestMapping("/")
+    public @ResponseBody String greeting() {
+        return "Hello, World!";
+    }
 
-@RequestMapping("/Laura")
-public @ResponseBody String greetingLaura() {
-return "Hello, Laura!";
-}
-@RequestMapping("/Thais")
-public @ResponseBody String greetingThais() {
-return "Hello, Thais!";
-}
+ 
+    @RequestMapping("/Emilly")
+    public @ResponseBody String greetingEmilly() {
+        return "Hello, Emilly!";
+    }
 
-}
+    @RequestMapping("/Ramide")
+    public @ResponseBody String greetingRamide() {
+        return "Hello, Ramide!";
+    }
+    // testando 
+
+    @RequestMapping("/Laura")
+    public @ResponseBody String greetingLaura() {
+        return "Hello, Laura!";
+    }
+
+    @RequestMapping("/Thais")
+    public @ResponseBody String greetingThais() {
+        return "Hello, Thais!";
+    }
+
+    @RequestMapping("/hotfix")
+    public @ResponseBody String greetingHotfix() {
+        return "Hello, hotfix!";
+    }
+  }
--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -57,4 +57,11 @@ class WebAppApplicationTests {
             .andExpect(status().isOk())  
             .andExpect(content().string(containsString("Hello, Thais!"))); 
     }
+    @Test
+    public void shouldReturnHotfixMessage() throws Exception {
+        this.mockMvc.perform(get("/Hotfix"))
+            .andDo(print())
+            .andExpect(status().isOk())  
+            .andExpect(content().string(containsString("Hello, hotfix!"))); 
+    }
 }
