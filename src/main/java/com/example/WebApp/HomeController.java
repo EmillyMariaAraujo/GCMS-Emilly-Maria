c501bb36404f78f2311ea2224a8e7aa700d89ed5
Emilly Maria Araujo da Silva <emillymaria93@gmail.com>
2025-07-30 22:55:20 -0300

    lo commit

---
+++ src/main/java/com/example/WebApp/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.WebApp;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+@RequestMapping("/")
+public @ResponseBody String greeting() {
+return "Hello, World!";
+}
+
+@RequestMapping("/Emilly")
+public @ResponseBody String greetingEmilly() {
+return "Hello, Emilly!";
+}
+
+@RequestMapping("/Ramide")
+public @ResponseBody String greetingRamide() {
+return "Hello, Ramide!";
+}
+}
---
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,45 @@
+package com.example.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WebAppApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/"))
+            .andDo(print())
+            .andExpect(status().isOk())  // Verifica se o status é 200 OK
+            .andExpect(content().string(containsString("Hello, World!")));  // Verifica o conteúdo da resposta
+    }
+
+    @Test
+    public void shouldReturnEmillyMessage() throws Exception {
+        this.mockMvc.perform(get("/Emilly"))
+            .andDo(print())
+            .andExpect(status().isOk())  // Verifica se o status é 200 OK
+            .andExpect(content().string(containsString("Hello, Emilly!")));  // Verifica o conteúdo da resposta
+    }
+
+  
+    @Test
+    public void shouldReturnRamideMessage() throws Exception {
+        this.mockMvc.perform(get("/Ramide"))
+            .andDo(print())
+            .andExpect(status().isOk())  // Verifica se o status é 200 OK
+            .andExpect(content().string(containsString("Hello, Ramide!")));  // Verifica o conteúdo da resposta
+    }
+}
